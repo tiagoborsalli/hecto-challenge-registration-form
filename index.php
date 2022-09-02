895a7613b4527a54c79e4a29e334d10949fc8b6e
Tiago Borsalli <tiagoborsalli@gmail.com>
2022-09-02 18:14:55 -0300

    v1.1
    Restructured the HTML layout from divs to table
    
    Finished the form validation

--- index.php
+++ index.php
@@ -14,6 +14,10 @@
     function validateForm() {
         let name = document.forms["reg"]["name"].value;
         let email = document.forms["reg"]["email"].value;
+        let cpf = document.forms["reg"]["id"].value;
+        let address = document.forms["reg"]["address"].value;
+        let city = document.forms["reg"]["city"].value;
+
         if (name == "") {
             alert("Name must be filled out");
             return false;
@@ -24,10 +28,26 @@
         function validateEmail(email) {
             let re = /\S+@\S+\.\S+/;
             return re.test(email);
-         }
+        }
     
         if (!validateEmail(email)) {
             alert('Input a legit email')
+        }
+
+        function validateCpf(cpf) {
+            let cpfRegex = /^[0-9]{3}.?[0-9]{3}.?[0-9]{3}-?[0-9]{2}/;
+            return cpfRegex.test(cpf);
+        };
+
+        if (!validateCpf(cpf)) {
+            alert('Input a legit ID/CPF')
+        }
+
+        if (address == "" || city == "") {
+            alert("Address and city must be filled out");
+            return false;
+        } else if (address.length < 3 || city.length < 3) {
+            alert("Address and city must be at least 3 characters");
         };
         
     };
@@ -38,10 +58,69 @@
     <div id="container">
         <div id="head">
             <img src="images/form-svgrepo-com.svg" />
-            <h1>Initial Registration Form v1.0</h1>
+            <h1>Initial Registration Form v1.1</h1>
         </div>
             <div class="jumbo">
-                <div id="label">
+                <table>
+                    <form name="reg" method="POST" action="" onsubmit="return validateForm()">
+                    <tr>
+                        <td>
+                            Name
+                        </td>
+                        <td>
+                            <input type="text" name="name" placeholder="name" value=""></input>
+                        </td>
+                        <td>
+
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Email
+                        </td>
+                        <td>
+                            <input type="text" id="email" name="email" placeholder="email"></input>
+                        </td>
+                        <td>
+
+                        </td>
+                        
+                    </tr>
+                    <tr>
+                        <td>
+                            ID (CPF)
+                        </td>
+                        <td>
+                            <input type="text" name="id" placeholder="CPF"></input>
+                        </td>
+                        <td>
+
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Address
+                        </td>
+                        <td>
+                            <input type="text" name="address" placeholder="address"></input>
+                        </td>
+                        <td>
+
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            City
+                        </td>
+                        <td>
+                            <input type="text" name="city" placeholder="city"></input>
+                        </td>
+                        <td>
+
+                        </td>
+                    </tr>
+                </table>
+               <!-- <div id="label">
                     Name
                     Email
                     ID (CPF)
@@ -55,7 +134,7 @@
                     <input type="text" name="id" placeholder="CPF"></input>
                     <input type="text" name="address" placeholder="address"></input>
                     <input type="text" name="city" placeholder="city"></input>
-                </div>
+                </div> -->
             </div>
             <div id="button">
                 <input type="submit" value="Submit" id="submit">
